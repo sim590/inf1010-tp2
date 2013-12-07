43d2a3c828501e667128c04bd9e4a897eab75576
fhamelin <fredhamelin91@gmail.com>
2013-12-06 20:00:42 -0500

    Enlever des appels à wrefresh() qui ne servaient à rien

--- src/client.c
+++ src/client.c
@@ -21,8 +21,6 @@ int main(void)
 
     wattron(displayWin, COLOR_PAIR(1));
     wattron(inputWin, COLOR_PAIR(2));
-    wrefresh(displayWin);
-    wrefresh(inputWin);
 
     inputCommand();
 
@@ -193,7 +191,6 @@ WINDOW *create_newwin(int height, int width, int starty, int startx)
     WINDOW *local_win;
 
     local_win = newwin(height, width, starty, startx);
-    wrefresh(local_win);
    
     return local_win;
 }
